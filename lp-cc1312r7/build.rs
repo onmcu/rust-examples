9ce21857bdc3e4a8cfad10721df1e4fcd111c43e
Joël Schulz-Andres <joel@systemscape.de>
2026-06-25 15:07:26 +0200

    chore: untrack lp-cc1312r7 (no support currently)

--- lp-cc1312r7/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search={manifest_dir}");
-    println!("cargo:rerun-if-changed=memory.x");
-}
